d0124019020bb183d53d44c26af3bc0452fca25b
lateralusd <xdaemonx@protonmail.ch>
2022-01-28 11:52:22 +0100

    Add reddit

--- cmd/run.go
+++ cmd/run.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/lateralusd/sochecker/plugins/github"
 	_ "github.com/lateralusd/sochecker/plugins/instagram"
 	_ "github.com/lateralusd/sochecker/plugins/pinterest"
+	_ "github.com/lateralusd/sochecker/plugins/reddit"
 	_ "github.com/lateralusd/sochecker/plugins/twitter"
 	"github.com/spf13/cobra"
 )
---
+++ plugins/reddit/reddit.go
@@ -0,0 +1,47 @@
+package reddit
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/lateralusd/sochecker/checker"
+)
+
+type RedditCheck struct {
+	link string
+}
+
+func (rc *RedditCheck) Check(client *http.Client, name string) bool {
+	checkurl := fmt.Sprintf("https://www.reddit.com/user/%s/", name)
+	rc.link = checkurl
+
+	req, err := http.NewRequest("GET", checkurl, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("User-Agent", "Instagram 10.3.2 (iPhone7,2; iPhone OS 9_3_3; en_US; en-US; scale=2.00; 750x1334) AppleWebKit/420+")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	b, _ := ioutil.ReadAll(resp.Body)
+	return !strings.Contains(string(b), "nobody on Reddit")
+}
+
+func (rc *RedditCheck) Info() string {
+	return "Check the username on reddit"
+}
+
+func (rc *RedditCheck) Link() string {
+	return rc.link
+}
+
+func init() {
+	checker.Register("reddit", &RedditCheck{})
+}
